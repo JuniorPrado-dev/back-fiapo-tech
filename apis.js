25bf1533a77498d393152f52d5b059ab4ea3ae25
Junior Prado <juniorcvnnn@gmail.com>
2022-09-22 15:58:59 -0300

    voltando para 3030 com chave certrobt

--- apis.js
+++ apis.js
@@ -14,10 +14,10 @@ app.get("/", (req, res) => {
 
 // Carrega o certificado e a key necessários para a configuração.
 const options = {
-  // key: fs.readFileSync("/etc/letsencrypt/live/api.fiapotech.com/privkey.pem"),
-  // cert: fs.readFileSync("/etc/letsencrypt/live/api.fiapotech.com/fullchain.pem")
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert")
+  key: fs.readFileSync("/etc/letsencrypt/live/api.fiapotech.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/api.fiapotech.com/fullchain.pem")
+  // key: fs.readFileSync("server.key"),
+  // cert: fs.readFileSync("server.cert")
 };
 
 const upload = require("multer")();
